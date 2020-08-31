f10146c947750d09b84fd2d665080ef90d6002ab
Alan Samet <usametov@gmail.com>
2020-08-31 00:16:52 -0400

    work on UI

--- src/cljs/bookmarks/views.cljs
+++ src/cljs/bookmarks/views.cljs
@@ -1,11 +1,36 @@
 (ns bookmarks.views
-  (:require [re-frame.core :as re-frame]
-            ; [cljs-react-material-ui.core :refer [get-mui-theme color]]
-            ; [cljs-react-material-ui.reagent :as ui]
-            ))
+  (:require 
+    [reagent.core  :as reagent]
+    [re-frame.core :as re-frame]            
+    [re-com.core   :refer [h-box v-box box gap line title label hyperlink-href input-textarea p p-span]]))
 
-; (defn navigate-to [link]
-;   (aset js/document "location" link))
+(defn github-hyperlink
+  "given a label and a relative path, return a component which hyperlinks to the GitHub URL in a new tab"
+  [label src-path]
+  (let [base-url (str "https://github.com/usametov/motherbox/tree/master/")]
+    [hyperlink-href
+     :label  label
+     ;:style  {:font-size    "13px"}
+     :href   (str base-url src-path)
+     :target "_blank"]))
+
+(defn panel-title
+  "Shown across the top of each page"
+  [panel-name box-style title-style]
+  [v-box   
+   :children [[h-box
+               :margin "4px 4px 9px 4px"
+               :height "54px"
+               :align :center               
+               :children [[title
+                           :label         panel-name
+                           :level         :level2
+                           :style         title-style
+                           :margin-bottom "0px"
+                           :margin-top    "2px"]
+                          [gap :size "25px"]]
+               :style box-style]
+              ]])
 
 (defn bookmark-card [bookmark]
   ; [ui/card {:class "bookmark-tile"}
@@ -28,23 +53,14 @@
 (defn bookmarks-cards []
  )
 
-
-
-; (defn submit-to-google [event]
-;   (.preventDefault event)
-;   (let [filter-value (re-frame/subscribe [:filter-value])]
-;     (navigate-to (str "https://www.google.de/search?q=" @filter-value))))
-
-(defn search-bar []
-    ; [:section {:class "search-bar"}
-    ;  [:form {:on-submit submit-to-google}
-    ;   [ui/text-field {:autofocus           "autofocus"
-    ;                   :floating-label-text "Search"
-    ;                   :hint-text           "Search bookmarks or press enter to google"
-    ;                   :id                  "search-input"
-    ;                   :full-width          true
-    ;                   :on-change           #(re-frame/dispatch [:filter-by (-> % .-target .-value)])}]]]
-)
+(defn search-bar []  
+  (let [search-term (reagent/atom nil)]
+    [input-textarea     
+     :model search-term
+     :width "480px"
+     :placeholder  "Search bookmarks"
+     :on-change    #(re-frame/dispatch [:filter-by search-term])
+     ]))
 
 (defn header []
   ; (let [name (re-frame/subscribe [:name])]
@@ -54,12 +70,21 @@
   )
 
 (defn main-panel []
+  [v-box   
+   :children [[panel-title "Bookmarks Explorer"
+               {:background-color "#008080" :padding-left "12px"} 
+               {:color "#fff" :font-weight "bold"}]
+              [line :color "#008080"]
+              [gap :size "15px"]
+              [search-bar]
+              [bookmarks-cards]
+              ]
+   ]
   ; [ui/mui-theme-provider
   ;  {:mui-theme (get-mui-theme {:palette {:primary1Color (color :teal500)}})}
   ;  [:div
   ;   [header]
   ;   [:main
-  ;    [search-bar]
-  ;    [bookmarks-cards]]]]
+  ; ]]]
   )
 
